bf38a6748d3971f3cee32e4bb4e37a58b4498d24
RorenNatsumi <RorenNatsumi@outlook.com>
2023-06-14 22:44:50 +0800

    封装soul_knight加密解密数据库

---
+++ main.py
@@ -0,0 +1,3 @@
+import soul_knight
+
+soul_knight.core.
---
+++ soul_knight/__init__.py
@@ -0,0 +1,3 @@
+# 这是一个元气骑士加密数据处理库，灵感来源于https://github.com/HTCheater/SKDEditor/
+# This is an encrypted data processing library for the Prime Knight, inspired by https://github.com/HTCheater/SKDEditor/
+# 使用soul_knight.core.Convert()进行操作
