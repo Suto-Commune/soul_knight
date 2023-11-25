d4b192c7a44ae1402c045e7c324943778170dcb8
Roren Natsumi <63963655+NatsumiXD@users.noreply.github.com>
2023-11-25 12:06:00 +0800

    Update example.py

--- example.py
+++ example.py
@@ -7,17 +7,24 @@ def find_files(path, patterns):
     for root, dirs, files in os.walk(path):
         for pattern_ in patterns:
             for filename in fnmatch.filter(files, pattern_):
-                yield os.path.join(root, filename)
-
+                if root == './com.ChillyRoom.DungeonShooter/files/encrypt' : continue
+                print(filename)
+                yield filename
+                #yield os.path.join(root, filename)
 
 USE = SKD.core.Convert()
 
-directory = './'
+#存档文件目录
+directory = './com.ChillyRoom.DungeonShooter/files/'
 
 pattern = ['*.data', "*.bytes"]
 
+abc = int(input("0解密 1加密："))
+
 for file_path in find_files(directory, pattern):
-    USE.FileName = file_path
-    print("---"+file_path+"---")
-    print(USE.de_open())
-    print("++++++++++++")
+    USE.FileName = directory + file_path
+    #USE.FileName_decrypt = directory + 'decrypt/' + file_path
+    USE.FileName_encrypt = directory + 'encrypt/' + file_path
+    if abc == 0 : USE.de_save() #解密 不可同时使用
+    if abc == 1 : USE.en_save() #加密 不可同时使用
+    #print(USE.de_open())
