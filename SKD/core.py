04c99432679ab27cc3d5a939dd667bf243627880
Roren <RorenNatsumi@outlook.com>
2024-06-08 20:13:23 +0800

    修复Bug

--- SKD/core.py
+++ SKD/core.py
@@ -1,167 +1,142 @@
 import base64
 import json
-import os
+import os.path
 import re
+
 from Crypto.Cipher import DES
 from Crypto.Util.Padding import pad, unpad
 
-# File 类：处理文件加密和解密逻辑。
-#
-# encrypt 方法：根据文件名选择合适的加密方式。
-# decrypt 方法：根据文件名选择合适的解密方式。
-# is_json_encrypted 方法：判断文件是否需要进行JSON加密。
-# get_key 方法：根据文件名获取对应的DES密钥。
-# decrypt_xml 方法：处理XML文件的解密。
-# encrypt_des 方法：使用DES算法加密数据。
-# decrypt_des 方法：使用DES算法解密数据。
-# xor 方法：使用异或操作加密/解密数据。
-# Config 类：管理配置，如加密目录路径。
-#
-# Convert 类：处理文件的读取、保存、加密和解密。
-#
-# de_open 方法：打开文件并解密。
-# de_save 方法：解密文件并保存为JSON格式。
-# en_save 方法：从JSON文件加密并保存。
-# encode 方法：使用 File 类的 encrypt 方法加密数据。
-# decode 方法：使用 File 类的 decrypt 方法解密数据。
-
 
 class File:
-    def __init__(self):
-        self.Const = self.Const()
+    def __init__(self) -> None:
+        pass
 
     class Const:
-        encryptedJsonGameFiles = [
-            "game.data", "item_data.data", "item_data_backups.bytes", "setting.data",
-            "statistic.data", "season_data.data", "season_data_backups.bytes",
-            "task.data"
-        ]
+        encryptedJsonGameFiles = ["game.data", "item_data.data", "item_data_backups.bytes", "setting.data",
+                                  "statistic.data", "season_data.data", "season_data_backups.bytes",
+                                  "task.data"]  # 添加其他文件名到列表中
 
     # 加密文件
     def encrypt(self, data, file_name):
         result = data.encode('utf-8')
-        if self.is_json_encrypted(file_name):
-            # 如果文件是JSON加密的，则将其压缩格式化
+        if ("battle" in file_name and "data" in file_name) or file_name in self.Const.encryptedJsonGameFiles:
             result = json.dumps(json.loads(data), separators=(',', ':')).encode('utf-8')
-        key = self.get_key(file_name)
-        if key:
-            # 如果有对应的DES密钥，则进行DES加密
-            result = self.encrypt_des(result, key)
+        if "item_data" in file_name or ("task" in file_name and "data" in file_name) or (
+                "setting" in file_name and "data" in file_name) or "season_data" in file_name:
+            result = self.encrypt_des(result, bytes([0x69, 0x61, 0x6d, 0x62, 0x6f, 0x0, 0x0, 0x0]))
+        if "statistic" in file_name and "data" in file_name:
+            result = self.encrypt_des(result, bytes([0x63, 0x72, 0x73, 0x74, 0x31, 0x0, 0x0, 0x0]))
         if "game" in file_name and "data" in file_name:
-            # 如果文件是游戏数据，则进行异或加密
             result = self.xor(result)
         return result
 
     # 解密文件
     def decrypt(self, data, file_name):
-        if file_name.endswith(".xml"):
-            # 如果是XML文件，使用特殊的XML解密方法
-            return self.decrypt_xml(data)
-        result = data
-        key = self.get_key(file_name)
-        if key:
-            # 如果有对应的DES密钥，则进行DES解密
-            result = self.decrypt_des(result, key)
-        if "game" in file_name and "data" in file_name:
-            # 如果文件是游戏数据，则进行异或解密
-            result = self.xor(result)
-        if self.is_json_encrypted(file_name):
-            # 如果文件是JSON加密的，则将其格式化
-            result = json.dumps(json.loads(result.decode('utf-8')), separators=(',', ':')).encode('utf-8')
-        return result.decode('utf-8')
-
-    def is_json_encrypted(self, file_name):
-        # 判断文件是否需要进行JSON加密
-        return ("battle" in file_name and "data" in file_name) or file_name in self.Const.encryptedJsonGameFiles
-
-    def get_key(self, file_name):
-        # 获取文件对应的DES密钥
+        result = data.decode('utf-8')
+        if ".xml" in file_name:
+            attrs_regex = re.compile("(<.*?/>|<.*?</.*?>)")
+            attrs_matcher = attrs_regex.finditer(data.decode('utf-8'))
+            attrs = []
+            final_xml = "<?xml version='1.0' encoding='utf-8' standalone='yes' ?>\n<map>"
+            for attrs_match in attrs_matcher:
+                for i in range(attrs_match.lastindex):
+                    attrs.append(attrs_match.group(i))
+            attrs.sort(key=lambda x: x.lower())
+            attrs_arr = attrs
+            for attr in attrs_arr:
+                final_xml += "\n    " + attr
+            final_xml += "\n</map>"
+            result = final_xml
         if "item_data" in file_name or ("task" in file_name and "data" in file_name) or (
                 "setting" in file_name and "data" in file_name) or "season_data" in file_name:
-            return bytes([0x69, 0x61, 0x6d, 0x62, 0x6f, 0x0, 0x0, 0x0])
+            result = self.decrypt_des(data, bytes([0x69, 0x61, 0x6d, 0x62, 0x6f, 0x0, 0x0, 0x0]))
         if "statistic" in file_name and "data" in file_name:
-            return bytes([0x63, 0x72, 0x73, 0x74, 0x31, 0x0, 0x0, 0x0])
-        return None
+            result = self.decrypt_des(data, bytes([0x63, 0x72, 0x73, 0x74, 0x31, 0x0, 0x0, 0x0]))
+        if "game" in file_name and "data" in file_name:
+            result = self.xor(data)
+        if ("battle" in file_name and "data" in file_name) or file_name in self.Const.encryptedJsonGameFiles:
+            result = json.dumps(json.loads(result), indent=4)
+        return result
 
-    def decrypt_xml(self, data):
-        # 解密XML文件
-        attrs_regex = re.compile("(<.*?/>|<.*?</.*?>)")
-        attrs_matcher = attrs_regex.finditer(data.decode('utf-8'))
-        attrs = [match.group(0) for match in attrs_matcher]
-        attrs.sort(key=lambda x: x.lower())
-        final_xml = "<?xml version='1.0' encoding='utf-8' standalone='yes' ?>\n<map>\n"
-        final_xml += "\n".join(attrs)
-        final_xml += "\n</map>"
-        return final_xml
+    @staticmethod
+    def xor(data):
+        key = bytes([115, 108, 99, 122, 125, 103, 117, 99, 127, 87, 109, 108, 107, 74, 95])
+        output = bytearray(len(data))
+        for i in range(len(data)):
+            output[i] = key[i % 15] ^ data[i]
+        return bytes(output)
 
-    def encrypt_des(self, data, key):
-        # 使用DES算法加密数据
-        cipher = DES.new(key, DES.MODE_ECB)
-        return base64.b64encode(cipher.encrypt(pad(data, DES.block_size)))
+    @staticmethod
+    def decrypt_des(data, key):
+        iv = bytes([0x41, 0x68, 0x62, 0x6f, 0x6f, 0x6c, 0x0, 0x0])
+        cipher_bytes = base64.b64decode(data)
+        cipher = DES.new(key, DES.MODE_CBC, iv)
+        result_bytes = cipher.decrypt(cipher_bytes)
+        result = unpad(result_bytes, DES.block_size).decode('utf-8')
+        return result
 
-    def decrypt_des(self, data, key):
-        # 使用DES算法解密数据
-        data = base64.b64decode(data)
-        cipher = DES.new(key, DES.MODE_ECB)
-        return unpad(cipher.decrypt(data), DES.block_size)
-
-    def xor(self, data):
-        # 使用异或操作加密/解密数据
-        key = b'WIZARD'
-        return bytes([b ^ key[i % len(key)] for i, b in enumerate(data)])
-
-
-class Config:
-    def __init__(self, encrypt_path='./com.ChillyRoom.DungeonShooter/files/encrypt'):
-        self.encrypt_path = encrypt_path
+    @staticmethod
+    def encrypt_des(data, key):
+        iv = bytes([0x41, 0x68, 0x62, 0x6f, 0x6f, 0x6c, 0x0, 0x0])
+        cipher = DES.new(key, DES.MODE_CBC, iv)
+        padded_data = pad(data, DES.block_size)
+        result_bytes = cipher.encrypt(padded_data)
+        result = base64.b64encode(result_bytes).decode('utf-8')
+        return result
 
 
 class Convert:
-    def __init__(self, config=Config()):
-        self.File = File()
-        self.config = config
-        self.FileName = ""
-        self.FileName_decrypt = ""
-        self.FileName_encrypt = ""
-        self.Id = 0
+    File = File()
+    FilePath = str()
+    FileName_decrypt = str()
+    FileName_encrypt = str()
+    ChillyRoom = './com.ChillyRoom.DungeonShooter/files/'
+    Id = int()
 
     def de_open(self):
-        # 打开文件并解密
-        with open(self.FileName, "rb") as f:
-            decode_data = self.decode(f.read())
+        f = open(self.FilePath, "rb")
+        decode_data = self.decode(f.read())
+        f.close()
         return decode_data
 
     def de_save(self):
-        # 解密文件并保存为JSON格式
-        with open(self.FileName, "rb") as f:
-            decode_data = self.decode(f.read())
-        with open(self.FileName + ".json", "w") as f:
-            will_write = json.dumps(json.loads(decode_data), ensure_ascii=False, indent=4)
-            f.write(will_write.encode("gbk", 'ignore').decode("gbk", "ignore"))
+        f = open(self.FilePath, "rb")
+        decode_data = self.decode(f.read())
+        f.close()
+        f = open(self.FilePath + ".json", "w")
+        will_write = json.dumps(json.loads(decode_data), ensure_ascii=False, indent=4)
+        # 加了忽略无法转换的gbk内容.encode("gbk", 'ignore').decode("gbk", "ignore")
+        f.write(will_write.encode("gbk", 'ignore').decode("gbk", "ignore"))
+        f.close()
         return will_write
 
     def en_save(self):
-        # 从JSON文件加密并保存
-        if not os.path.exists(self.FileName + ".json"):
-            print("找不到文件 " + self.FileName + "。请检查文件是否存在，或者使用 de_save() 创建它。")
-            return "文件未找到。"
-        with open(self.FileName + ".json", "r") as f:
-            encode_data = json.dumps(json.loads(f.read()), ensure_ascii=False, separators=(',', ':'))
-            encode_data = self.encode(encode_data)
+        if not os.path.exists(self.FilePath + ".json"):
+            print(
+                "Cannot ind the file called " + self.FilePath + ".Check if the file exists or use de_save() to create "
+                                                                "it.")
+            return "File Not Found."
+        f = open(self.FilePath + ".json", "r")
+        encode_data = json.dumps(json.loads(f.read()), ensure_ascii=False, separators=(',', ':'))
 
+        encode_data = self.encode(encode_data)
+        f.close()
         # 判断是否创建文件夹
-        if not os.path.exists(self.config.encrypt_path):
-            os.makedirs(self.config.encrypt_path)
-
-        mode = "wb" if "game" in self.FileName and "data" in self.FileName else "w"
-        with open(self.FileName_encrypt, mode) as f:
-            f.write(encode_data)
-        return encode_data
+        if not os.path.exists(self.ChillyRoom+'/encrypt/'):
+            os.makedirs(self.ChillyRoom+'/encrypt/')
+        f = open(self.ChillyRoom+'/encrypt/'+self.FileName_encrypt, "w")
+        # game单独的读写
+        if "game" in self.FilePath and "data" in self.FilePath:
+            f = open(self.ChillyRoom+'/encrypt/'+self.FileName_encrypt, "wb")
+        will_write = encode_data
+        f.write(will_write)
+        f.close()
+        return will_write
 
     def encode(self, data: str):
-        # 使用 File 类的 encrypt 方法加密数据
-        return self.File.encrypt(data, self.FileName)
+        byte = data
+        return self.File.encrypt(byte, self.FilePath)
 
     def decode(self, data: bytes):
-        # 使用 File 类的 decrypt 方法解密数据
-        return self.File.decrypt(data, self.FileName)
+        byte = data
+        return self.File.decrypt(byte, self.FilePath)
--- example.py
+++ example.py
@@ -7,28 +7,28 @@ def find_files(path, patterns):
     for root, dirs, files in os.walk(path):
         for pattern_ in patterns:
             for filename in fnmatch.filter(files, pattern_):
-                if root == './com.ChillyRoom.DungeonShooter/files/encrypt' : continue
                 print(filename)
                 yield filename
-                #yield os.path.join(root, filename)
+                # yield os.path.join(root, filename)
+
 
 USE = SKD.core.Convert()
 
-#存档文件目录
-directory = './com.ChillyRoom.DungeonShooter/files/'
-try:
-    os.mkdir("jiemi")
-except:
-    ...
-config=SKD.core.Config(encrypt_path="./jiemi")
+# 存档文件目录
+# 目录结尾一定要加上'/'
+directory = './data/'
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
 pattern = ['*.data', "*.bytes"]
 
 abc = int(input("0解密 1加密："))
 
 for file_path in find_files(directory, pattern):
-    USE.FileName = directory + file_path
-    #USE.FileName_decrypt = directory + 'decrypt/' + file_path
-    USE.FileName_encrypt = directory + 'encrypt/' + file_path
-    if abc == 0 : USE.de_save() #解密 不可同时使用
-    if abc == 1 : USE.en_save() #加密 不可同时使用
-    #print(USE.de_open())
+    USE.FilePath = directory + file_path
+    USE.FileName_encrypt = file_path
+    if abc == 0:
+        USE.de_save()  # 解密 不可同时使用
+    if abc == 1:
+        USE.en_save()  # 加密 不可同时使用
+    # print(USE.de_open())
